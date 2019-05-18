c9690570b6eb4ce5ce7ff2e0630e95e0ca10ba43
CallMeGwei <34364155+CallMeGwei@users.noreply.github.com>
2019-05-18 18:35:08 -0500

    Inadvertently rewrote commit history. Reduce complexity. Remove commit/reveal that would allow arbitrary facilitator.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -18,14 +18,11 @@ contract Remittance is OwnablePausable {
         // keep track of the amount that was sent
         uint sentAmount;
 
-        // fee user will pay to facilitate the remittance
+        // fee user will pay to contract to structure the remittance
         uint feeAmount;
 
         // keep track of sending party in case of refund
         address payable sendingParty;
-
-        // users will need to prove they have access to some data that hashes to this
-        bytes32 hashedSecret;
 
         // time limits and expirations
         uint whenTxRedeemable;
@@ -33,8 +30,6 @@ contract Remittance is OwnablePausable {
     }
 
     mapping (bytes32 => RemittanceTx) public remittances;
-    mapping (uint => bytes32) public remittanceIdsToHashes;
-    mapping (bytes32 => bytes32) public completionCommitments;
 
     event LogChangedFeeAmount(address whoChanged, uint newFeeAmount);
     event LogCreatedRemittanceTx(address whoSent, uint whatValue, uint whatFee, address allowedIntermediary);
@@ -56,14 +51,16 @@ contract Remittance is OwnablePausable {
 
     function changeFeeAmount(uint newFeeAmount) public onlyOwner softPausable {
 
+        // fee set here goes to contract owner, not to remittance facilitator
         emit LogChangedFeeAmount(msg.sender, newFeeAmount);
         currentFlatFee = newFeeAmount;
 
     }
 
-    function createNewRemittanceTx(bytes32 hashedSecret, uint txRedeemableTime, uint txExpirationTime) public softPausable payable returns(uint){
+    // The hashedSecretAsID comes from a hashed password and a facilitator destination address. It is computed on the front end.
+    function createNewRemittanceTx(bytes32 hashedSecretAsID, uint txRedeemableTime, uint txExpirationTime) public softPausable payable {
 
-            require(msg.value > currentFlatFee, "You must send at least more ether than the fee.");
+            require(msg.value > currentFlatFee, "You must, at least, send enough ether to cover the fee.");
 
             // block timestamps seem reasonable where +- 30 seconds won't interfere with the intention of the user
             require(txExpirationTime > txRedeemableTime, "RemittanceTx would expire before it was redeemable.");
@@ -71,89 +68,57 @@ contract Remittance is OwnablePausable {
             require(txRedeemableTime < block.timestamp.add(_maxFutureTime), "Redeemable time is too far in the future.");
             require(txExpirationTime < block.timestamp.add(_maxExpiresTime), "Expiration time is too far in the future.");
 
-            bytes32 remittanceIdHash = keccak256(abi.encodePacked(idCount, hashedSecret));
-            RemittanceTx storage newRemittance = remittances[remittanceIdHash];
-
-            remittanceIdsToHashes[idCount] = remittanceIdHash;
-            idCount = idCount.add(1);
+            RemittanceTx storage newRemittance = remittances[hashedSecretAsID];
 
             newRemittance.sentAmount = msg.value;
             newRemittance.feeAmount = currentFlatFee;
             newRemittance.sendingParty = msg.sender;
-            newRemittance.hashedSecret = hashedSecret; // hashed secret derived from sender and receiver secrets
-
             newRemittance.whenTxRedeemable = txRedeemableTime;
             newRemittance.whenTxExpires = txExpirationTime;
 
             emit LogLockedBalance(msg.value);
             lockedBalance = lockedBalance.add(msg.value);
-
-            return idCount.sub(1);
     }
 
-    function getRemittanceTxFromId(uint remittanceId) internal view returns(RemittanceTx memory){
-        bytes32 remittanceHashId = remittanceIdsToHashes[remittanceId];
-        require(remittanceHashId != 0x0, "No outstanding transaction matches details.");
+    function refundExpiredRemittanceTx(bytes32 hashedSecretAsID) public softPausable {
 
-        return remittances[remittanceHashId];
-    }
-
-    function refundExpiredRemittanceTx(uint remittanceId) public softPausable {
-
-        RemittanceTx memory aRemittance = getRemittanceTxFromId(remittanceId);
+        RemittanceTx memory aRemittance = remittances[hashedSecretAsID];
 
         require(block.timestamp >= aRemittance.whenTxExpires, "Transaction has not yet expired. Ineligable for refund.");
 
         emit LogRemittanceRefunded(msg.sender, aRemittance.sendingParty, aRemittance.sentAmount);
         emit LogUnlockedBalance(aRemittance.sentAmount);
 
-         // remove the outstanding transaction
-        delete(remittances[remittanceIdsToHashes[remittanceId]]);
-        delete(remittanceIdsToHashes[remittanceId]);
+         // remove the transaction record
+        delete(remittances[hashedSecretAsID]);
 
         // and update the locked balance
         lockedBalance = lockedBalance.sub(aRemittance.sentAmount);
 
-        // refund the transaction to original sender
+        // finally, refund the transaction to original sender
         aRemittance.sendingParty.transfer(aRemittance.sentAmount);
     }
 
-    // potential intermediaries will need to submit a secretVerification
-    // using web3.utils.soliditySha3(the_secret, some_salt) or similar
-    // the_secret is itself the hash of the sending and receiving parties.
-    function commitToCompleteRemittanceTx(uint remittanceId, bytes32 secretVerification) public softPausable {
-        completionCommitments[keccak256(abi.encodePacked(remittanceId, msg.sender))] = secretVerification;
-    }
+    function completeRemittanceTx(bytes32 hashedSecret) public softPausable {
 
-    // after committing the secretVerification hash, the intermediary can complete the remittance
-    // assuming their revealed secret matches their verification and the actual secret for the remittance
-    function completeRemittanceTx(uint remittanceId, bytes32 secret, bytes32 salt) public softPausable {
+        bytes32 secretVerification = keccak256(abi.encodePacked(hashedSecret, msg.sender));
 
-        bytes32 secretVerification = completionCommitments[keccak256(abi.encodePacked(remittanceId, msg.sender))];
-
-        require(secretVerification != 0x0, "You need to commit to complete this remittance before you can actually complete it.");
-
-        RemittanceTx memory aRemittance = getRemittanceTxFromId(remittanceId);
+        RemittanceTx memory aRemittance = remittances[secretVerification];
 
         require(aRemittance.sentAmount > 0, "Transaction has a zero-value.");
         require(aRemittance.whenTxRedeemable < block.timestamp, "Transaction is not yet redeemable.");
-
-        require(secretVerification == keccak256(abi.encodePacked(secret, salt)), "Secret verification from commitment mismatch!");
-        require(aRemittance.hashedSecret == keccak256(abi.encodePacked(secret)), "Secret mismatch!");
 
         emit LogUnlockedBalance(aRemittance.sentAmount);
         emit LogRemittanceCompleted(msg.sender, aRemittance.feeAmount, aRemittance.sentAmount.sub(aRemittance.feeAmount));
 
          // remove the outstanding transaction
-        delete(remittances[remittanceIdsToHashes[remittanceId]]);
-        delete(remittanceIdsToHashes[remittanceId]);
-        delete(secretVerification);
+        delete(remittances[secretVerification]);
 
         // and update the locked balance
         lockedBalance = lockedBalance.sub(aRemittance.sentAmount);
 
-        // send amount to handler
-        msg.sender.transfer(aRemittance.sentAmount);
+        // send amount, less remittance contract use fee, to facilitator
+        msg.sender.transfer(aRemittance.sentAmount.sub(aRemittance.feeAmount));
     }
 
     function withdrawFromUnlockedBalance(uint amountRequested, address payable whereTo) public onlyOwner softPausable {
